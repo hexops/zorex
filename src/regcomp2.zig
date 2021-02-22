9904d6720c16ff2cf0913f67fc0f2e3c8b3ac973
Stephen Gutekanst <stephen@hexops.com>
2021-02-22 03:00:08 -0700

    begin to translate ops_init
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/regcomp2.zig
+++ src/regcomp2.zig
@@ -134,40 +134,30 @@ const config = @import("config.zig");
 // }
 // #endif
 
-// static int
-// ops_init(regex_t* reg, int init_alloc_size)
-// {
-//   Operation* p;
-//   size_t size;
-
-//   if (init_alloc_size > 0) {
+pub fn opsInit(allocator: *Allocator, reg: *Regex, init_alloc_size: usize) !void {
+    if (init_alloc_size > 0) {
 //     size = sizeof(Operation) * init_alloc_size;
 //     p = (Operation* )xrealloc(reg->ops, size);
 //     CHECK_NULL_RETURN_MEMERR(p);
 //     reg->ops = p;
-// #ifdef USE_DIRECT_THREADED_CODE
-//     {
+        if (config.UseDirectThreadedCode) {
 //       enum OpCode* cp;
 //       size = sizeof(enum OpCode) * init_alloc_size;
 //       cp = (enum OpCode* )xrealloc(reg->ocs, size);
 //       CHECK_NULL_RETURN_MEMERR(cp);
 //       reg->ocs = cp;
-//     }
-// #endif
-//   }
-//   else {
-//     reg->ops = (Operation* )0;
-// #ifdef USE_DIRECT_THREADED_CODE
-//     reg->ocs = (enum OpCode* )0;
-// #endif
-//   }
-
-//   reg->ops_curr  = 0; /* !!! not yet done ops_new() */
-//   reg->ops_alloc = init_alloc_size;
-//   reg->ops_used  = 0;
-
-//   return ONIG_NORMAL;
-// }
+        }
+    } else {
+        reg.ops = null;
+        if (config.UseDirectThreadedCode) {
+            reg.ocs = null;
+        }
+    }
+    reg.re_pattern_buffer.ops_curr = 0; // !!! not yet done opsNew()
+    reg.re_pattern_buffer.ops_alloc = init_alloc_size;
+    reg.re_pattern_buffer.ops_used = 0;
+    return;
+}
 
 // static int
 // ops_expand(regex_t* reg, int n)
