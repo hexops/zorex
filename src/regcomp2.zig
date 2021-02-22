793d4aa4a3c7883d3c87b6ec972765858786c7a2
Stephen Gutekanst <stephen@hexops.com>
2021-02-21 22:47:59 -0700

    translate onig_new to Zig
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/regcomp2.zig
+++ src/regcomp2.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
 // /**********************************************************************
 //   regcomp.c -  Oniguruma (regular expression library)
 // **********************************************************************/
@@ -7666,30 +7669,11 @@
 //   return r;
 // }
 
-// extern int
-// onig_new(regex_t** reg, const UChar* pattern, const UChar* pattern_end,
-//          OnigOptionType option, OnigEncoding enc, OnigSyntaxType* syntax,
-//          OnigErrorInfo* einfo)
-// {
-//   int r;
-
-//   *reg = (regex_t* )xmalloc(sizeof(regex_t));
-//   if (IS_NULL(*reg)) return ONIGERR_MEMORY;
-
-//   r = onig_reg_init(*reg, option, ONIGENC_CASE_FOLD_DEFAULT, enc, syntax);
-//   if (r != 0) {
-//     xfree(*reg);
-//     *reg = NULL;
-//     return r;
-//   }
-
-//   r = onig_compile(*reg, pattern, pattern_end, einfo);
-//   if (r != 0) {
-//     onig_free(*reg);
-//     *reg = NULL;
-//   }
-//   return r;
-// }
+pub fn New(allocator: *Allocator, pattern: []u8, option: Option, syntax: Syntax) void! {
+    const reg = try Regex.init(allocator, option, ONIGENC_CASE_FOLD_DEFAULT, syntax);
+    errdefer reg.deinit();
+    try reg.compile(pattern)
+}
 
 // extern int
 // onig_initialize(OnigEncoding encodings[], int n)
