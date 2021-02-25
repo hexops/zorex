96cbd1c150cebcb0dc44de09a993307fb1532dac
Stephen Gutekanst <stephen@hexops.com>
2021-02-25 00:43:57 -0700

    remove license headers (will put in LICENSE file)
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/regexec.zig
+++ src/regexec.zig
@@ -2,43 +2,6 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ReRegisters = @import("oniguruma2.zig").ReRegisters;
 
-// /**********************************************************************
-//   regexec.c -  Oniguruma (regular expression library)
-// **********************************************************************/
-// /*-
-//  * Copyright (c) 2002-2021  K.Kosako
-//  * All rights reserved.
-//  *
-//  * Redistribution and use in source and binary forms, with or without
-//  * modification, are permitted provided that the following conditions
-//  * are met:
-//  * 1. Redistributions of source code must retain the above copyright
-//  *    notice, this list of conditions and the following disclaimer.
-//  * 2. Redistributions in binary form must reproduce the above copyright
-//  *    notice, this list of conditions and the following disclaimer in the
-//  *    documentation and/or other materials provided with the distribution.
-//  *
-//  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-//  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-//  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-//  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
-//  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-//  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-//  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-//  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-//  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-//  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-//  * SUCH DAMAGE.
-//  */
-
-// #ifndef ONIG_NO_PRINT
-// #ifndef NEED_TO_INCLUDE_STDIO
-// #define NEED_TO_INCLUDE_STDIO
-// #endif
-// #endif
-
-// #include "regint.h"
-
 // #define IS_MBC_WORD_ASCII_MODE(enc,s,end,mode) \
 //   ((mode) == 0 ? ONIGENC_IS_MBC_WORD(enc,s,end) : ONIGENC_IS_MBC_WORD_ASCII(enc,s,end))
 
