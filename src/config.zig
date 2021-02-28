8c83acda9b9154d664becc0d6792dd7b36c8cbe6
Stephen Gutekanst <stephen@hexops.com>
2021-02-27 22:39:37 -0700

    remove all
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/config.zig
+++
@@ -1,4 +0,0 @@
-pub const Debug = false;
-pub const DebugParse = false;
-pub const DebugNodeFree = false;
-pub const UseDirectThreadedCode = false; // TODO(slimsag): confirm default
--- src/main.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-pub const New = @import("regcomp.zig").New;
-pub const ReRegisters = @import("oniguruma.zig").ReRegisters;
-pub const Option = @import("oniguruma.zig").Option;
-pub const Syntax = @import("regsyntax.zig").Syntax;
-pub const Region = @import("regexec.zig").Region;
-pub const search = @import("regexec.zig").search;
-
