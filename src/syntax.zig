7cadb77da37800fad1b072399cb907999155f9e1
Stephen Gutekanst <stephen@hexops.com>
2021-03-02 13:18:40 -0700

    add escaped literal parsing
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/syntax.zig
+++ src/syntax.zig
@@ -30,38 +30,27 @@ pub fn regex(comptime Reader: type) comb.Parser(RegexNode, Reader) {
 }
 
 /// returns a parser for parsing escaped regex literals (e.g. `\*`).
-pub fn escapedLiteral(comptime Reader: type) comb.Parser(u8, Reader) {
-    return struct {
-        pub const parser = comb.Parser(u8, Reader){.parse = parse};
-
-        /// parses regex syntax.
-        ///
-        /// Caller is responsible for calling deinit() on the result, if any.
-        fn parse(allocator: *Allocator, src: *Reader) callconv(.Inline) comb.Error!?u8 {
-            // Read the backslash "\" escape character.
-            const want = '\\';
-            const buf: [1]u8;
-            const read = try src.reader().readAll(&buf);
-            if (read < want.len or !std.mem.eql(u8, buf, want)) {
-                try src.seekableStream().seekBy(-@intCast(i64, read));
-                return null;
-            }
-
-            // Read the next (escaped) character.
-            const read = try src.reader().readAll(&buf);
-            if (read < want.len) {
-                try src.seekableStream().seekBy(-@intCast(i64, read));
-                return null;
-            }
-            return buf[0];
-        }
-    }.parser;
+pub fn escapedLiteral(comptime Reader: type) comb.Parser([]comb.Rune, Reader) {
+    return comb.sequence(comb.Rune, Reader, .{
+        comb.rune(comb.Rune.fromString("\\") catch unreachable, Reader),
+        comb.anyRune(Reader),
+    });
 }
-
 
 test "literal" {
     const allocator = std.testing.allocator;
     var reader = std.io.fixedBufferStream("abcdef");
     const syntaxTree = try regex(@TypeOf(reader)).parse(allocator, &reader);
     std.debug.print("result: {any}\n", .{syntaxTree.?});
+}
+
+test "escapedLiteral" {
+    const allocator = std.testing.allocator;
+    var reader = std.io.fixedBufferStream("\\abcdef");
+
+    var result = try escapedLiteral(@TypeOf(reader)).parse(allocator, &reader);
+    defer allocator.free(result.?);
+    const str = try comb.Rune.toString(allocator, result.?);
+    defer allocator.free(str);
+    std.testing.expectEqualStrings("\\a", str);
 }
