33e31d0fc8f55b5127f32035da136fb6bdc2fba7
Stephen Gutekanst <stephen@hexops.com>
2021-03-13 15:39:48 -0700

    parser combinators with runtime parser generation
    As outlined in https://devlog.hexops.com/2021/zig-parser-combinators-and-why-theyre-awesome
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,8 @@
 const std = @import("std");
-const regex = @import("syntax.zig").regex;
+const parser = @import("parser.zig");
+const syntax = @import("syntax.zig");
 
-test "syntax_parsing" {
-    const allocator = std.testing.allocator;
-    var reader = std.io.fixedBufferStream("abcdef");
-    const syntaxTree = try regex(@TypeOf(reader)).parse(allocator, &reader);
-    defer syntaxTree.?.deinit(allocator);
-
-    var buf = std.ArrayList(u8).init(allocator);
-    defer buf.deinit();
-    try syntaxTree.?.writeTo(buf.writer());
-    std.testing.expectEqualStrings("abcdef", buf.items);
+test "parser" {
+    _ = parser;
+    _ = syntax;
 }
--- src/syntax.zig
+++ src/syntax.zig
@@ -1,245 +1,86 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const comb = @import("combinator.zig");
+const Parser = @import("parser.zig").Parser;
+const OneOf = @import("parser.zig").OneOf;
+const Error = @import("parser.zig").Error;
 
-const NodeDataTag = enum {
-    escapedLiteral,
-    literal,
-};
+pub fn Literal(comptime Reader: type) type {
+    return struct {
+        parser: Parser([]u8, Reader) = .{
+            ._parse = parse,
+        },
+        want: []const u8,
 
-pub const NodeData = union(NodeDataTag) {
-    /// An escaped metacharacter / literal, e.g. `\.\*`. Escape characters are included.
-    escapedLiteral: []const u8,
+        const Self = @This();
 
-    /// A literal, e.g. `foobar`
-    literal: []const u8,
-
-    pub fn deinit(self: NodeData, allocator: *Allocator) void {
-        switch (self) {
-            .escapedLiteral => allocator.free(self.escapedLiteral), 
-            .literal => allocator.free(self.literal),
+        pub fn init(want: []const u8) Self {
+            return Self{
+                .want = want
+            };
         }
-    }
 
-    pub fn writeTo(self: NodeData, writer: anytype) !void {
-        switch (self) {
-            .escapedLiteral => try std.fmt.format(writer, "{s}", .{self.escapedLiteral}),
-            .literal => try std.fmt.format(writer, "{s}", .{self.literal}),
-        }
-    }
-};
-
-/// A repetition operator, e.g. `*`, `+`, `?`
-pub const RepetitionOperator = struct {
-    /// Minimum number of times to expect sequence.
-    min: usize,
-
-    /// Maximum number of times to expect sequence, or -1 for unlimited.
-    max: isize,
-
-    pub fn writeTo(self: RepetitionOperator, writer: anytype) !void {
-        if (self.min == 0 and self.max == -1) {
-            // zero or more, prefer more.
-            return try std.fmt.format(writer, "*", .{});
-        } else if (self.min == 1 and self.max == -1) {
-            // one or more, prefer more.
-            return try std.fmt.format(writer, "+", .{});
-        } else if (self.min == 0 and self.max == 1) {
-            // zero or one, prefer one.
-            return try std.fmt.format(writer, "?", .{});
-        } else if (self.min == self.max) {
-            // exactly N
-            return try std.fmt.format(writer, "{{d}}", .{self.min});
-        } else if (self.max == -1) {
-            // N or more, prefer more.
-            return try std.fmt.format(writer, "{{d},}", .{self.min});
-        }
-        // [min, max] number of matches, prefer more.
-        return try std.fmt.format(writer, "{{d},{d}}", .{self.min, self.max});
-        // TODO(slimsag): future: support non-greedy variants
-        // x*?, x+?, x??, x{n,m}?, x{n,}?, x{n}?
-    }
-};
-
-pub const Node = struct {
-    data: NodeData,
-    children: std.ArrayList(*Node),
-    repetition: ?RepetitionOperator,
-
-    pub fn init(allocator: *Allocator, data: NodeData) !*Node {
-        const n = try allocator.create(Node);
-        n.* = Node{
-            .data = data,
-            .children = std.ArrayList(*Node).init(allocator),
-            .repetition = null,
-        };
-        return n;
-    }
-
-    pub fn deinit(self: *Node, allocator: *Allocator) void {
-        self.data.deinit(allocator);
-        for (self.children.items) |child| {
-            child.deinit(allocator);
-        }
-        self.children.deinit();
-        allocator.destroy(self);
-    }
-
-    pub fn writeTo(self: *Node, writer: anytype) !void {
-        try self.data.writeTo(writer);
-    }
-};
-
-/// returns a parser for parsing regex syntax.
-pub fn regex(comptime Reader: type) comb.Parser(*Node, Reader) {
-    return comb.oneOf(*Node, Reader, .{
-        escapedLiteral(Reader),
-        unescapedLiteral(Reader),
-    });
-}
-
-fn repetitionOperator(comptime Reader: type) comb.Parser(RepetitionOperator, Reader) {
-    return comb.oneOf(RepetitionOperator, Reader, .{
-        repetitionOperatorSimple(Reader, "*", .{.min = 0, .max = -1}),
-        repetitionOperatorSimple(Reader, "+", .{.min = 1, .max = -1}),
-        repetitionOperatorSimple(Reader, "?", .{.min = 0, .max = 1}),
-        // TODO(slimsag): handle x{n}, x{n,m}, x{n,}
-    });
-}
-
-fn repetitionOperatorSimple(comptime Reader: type, rune: []const u8, value: RepetitionOperator) comb.Parser(RepetitionOperator, Reader) {
-    const v = struct {
-        pub const parser = comb.Parser(RepetitionOperator, Reader){ .parse = parse };
-        pub const target: comb.Rune;
-        pub const mapTo: RepetitionOperator;
-
-        fn parse(allocator: *Allocator, src: *Reader) callconv(.Inline) comb.Error!?RepetitionOperator {
-            const v = try target.parse(allocator, src);
-            if (v == null) {
+        fn parse(parser: *Parser([]u8, Reader), allocator: *Allocator, src: *Reader) callconv(.Inline) Error!?[]u8 {
+            const self = @fieldParentPtr(Self, "parser", parser);
+            const buf = try allocator.alloc(u8, self.want.len);
+            const read = try src.reader().readAll(buf);
+            if (read < self.want.len or !std.mem.eql(u8, buf, self.want)) {
+                try src.seekableStream().seekBy(-@intCast(i64, read));
+                allocator.free(buf);
                 return null;
             }
-            return mapTo;
+            return buf;
         }
     };
-    v.target = comb.rune(comb.Rune.fromString(rune) catch unreachable, Reader);
-    v.mapTo = value;
-    return v.parser;
-}
-
-/// returns a parser for parsing one or more escaped regex literals (e.g. `\*`),
-/// followed by an optional repetition operator.
-fn escapedLiteral(comptime Reader: type) comb.Parser(*Node, Reader) {
-    return struct {
-        pub const parser = comb.Parser(*Node, Reader){ .parse = parse };
-
-        /// parses an escaped literal node.
-        ///
-        /// Caller is responsible for calling deinit() on the result, if any.
-        fn parse(allocator: *Allocator, src: *Reader) callconv(.Inline) comb.Error!?*Node {
-            const inner = comb.repeated(
-                []comb.Rune,
-                Reader,
-                1,
-                -1,
-                comb.sequence(comb.Rune, Reader, .{
-                    comb.rune(comb.Rune.fromString("\\") catch unreachable, Reader),
-                    comb.anyRune(Reader),
-                }),
-            );
-
-            const v = try inner.parse(allocator, src);
-            if (v == null) {
-                return null;
-            }
-            defer v.?.deinit();
-            var strLen: usize = 0;
-            for (v.?.items) |runes| {
-                for (runes) |rune| {
-                    strLen += rune.len;
-                }
-            }
-            var str = try allocator.alloc(u8, strLen);
-            var i: usize = 0;
-            for (v.?.items) |runes| {
-                for (runes) |rune| {
-                    std.mem.copy(u8, str[i .. i + rune.len], rune.buf[0..rune.len]);
-                    i += rune.len;
-                }
-                allocator.free(runes);
-            }
-            return Node.init(allocator, .{ .escapedLiteral = str });
-        }
-    }.parser;
-}
-
-/// returns a parser for parsing one or more unescaped regex literals (e.g. `abc123`)
-/// without parsing regex metacharacters (e.g. `*`), followed by an optional repetition
-/// operator.
-fn unescapedLiteral(comptime Reader: type) comb.Parser(*Node, Reader) {
-    return struct {
-        pub const parser = comb.Parser(*Node, Reader){ .parse = parse };
-
-        /// parses an unescaped literal node.
-        ///
-        /// Caller is responsible for calling deinit() on the result, if any.
-        fn parse(allocator: *Allocator, src: *Reader) callconv(.Inline) comb.Error!?*Node {
-            const inner = comb.repeated(
-                comb.Rune,
-                Reader,
-                1,
-                -1,
-                comb.nextIfNot(
-                    comb.Rune,
-                    Reader,
-                    // any rune
-                    comb.anyRune(Reader),
-                    // not preceded by these:
-                    comb.oneOf(comb.Rune, Reader, .{
-                        comb.rune(comb.Rune.fromString("\\") catch unreachable, Reader),
-                    }),
-                ),
-            );
-
-            const v = try inner.parse(allocator, src);
-            if (v == null) {
-                return null;
-            }
-            defer v.?.deinit();
-            var strLen: usize = 0;
-            for (v.?.items) |rune| {
-                strLen += rune.len;
-            }
-            var str = try allocator.alloc(u8, strLen);
-            var i: usize = 0;
-            for (v.?.items) |rune| {
-                std.mem.copy(u8, str[i .. i + rune.len], rune.buf[0..rune.len]);
-                i += rune.len;
-            }
-            return Node.init(allocator, .{ .literal = str });
-        }
-    }.parser;
 }
 
 test "literal" {
     const allocator = std.testing.allocator;
     var reader = std.io.fixedBufferStream("abcdef");
-    const syntaxTree = try regex(@TypeOf(reader)).parse(allocator, &reader);
-    defer syntaxTree.?.deinit(allocator);
-
-    var buf = std.ArrayList(u8).init(allocator);
-    defer buf.deinit();
-    try syntaxTree.?.writeTo(buf.writer());
-    std.testing.expectEqualStrings("abcdef", buf.items);
+    var want: []const u8 = "abc";
+    var literal = Literal(@TypeOf(reader)).init(want);
+    const p = &literal.parser;
+    var result = try p.parse(allocator, &reader);
+    std.testing.expectEqualStrings(want, result.?);
+    if (result) |r| {
+        allocator.free(r);
+    }
 }
 
-test "escapedLiteral" {
+test "oneof_literal" {
     const allocator = std.testing.allocator;
-    var reader = std.io.fixedBufferStream("\\a\\bcdef");
-    const syntaxTree = try regex(@TypeOf(reader)).parse(allocator, &reader);
-    defer syntaxTree.?.deinit(allocator);
+    var reader = std.io.fixedBufferStream("catdogsheep");
 
-    var buf = std.ArrayList(u8).init(allocator);
-    defer buf.deinit();
-    try syntaxTree.?.writeTo(buf.writer());
-    std.testing.expectEqualStrings("\\a\\b", buf.items);
+    // Define our parser.
+    var one_of = OneOf([]u8, @TypeOf(reader)).init(&.{
+        &Literal(@TypeOf(reader)).init("dog").parser,
+        &Literal(@TypeOf(reader)).init("sheep").parser,
+        &Literal(@TypeOf(reader)).init("cat").parser,
+    });
+    var p = &one_of.parser;
+
+    // Parse!
+    var result = try p.parse(allocator, &reader);
+    std.testing.expectEqualStrings("cat", result.?);
+    if (result) |r| {
+        allocator.free(r);
+    }
 }
+
+// comptime parsing will be possible once there is a comptime allocator:
+// https://github.com/ziglang/zig/issues/1291
+// test "literal_comptime" {
+//     comptime {
+//         var buffer: [10*1024*1024]u8 = undefined;
+//         var fixed = std.heap.FixedBufferAllocator.init(buffer[0..]);
+//         var allocator = &fixed.allocator;
+//         var reader = std.io.fixedBufferStream("abcdef");
+//         var want: []const u8 = "abc";
+//         var literal = Literal(@TypeOf(reader)).init(want);
+//         const p = &literal.parser;
+//         var result = try p.parse(allocator, &reader);
+//         std.testing.expectEqualStrings(want, result.?);
+//         if (result) |r| {
+//             allocator.free(r);
+//         }
+//     }
+// }
