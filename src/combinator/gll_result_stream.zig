c49ea273fd0be0653a31bf6ba304942034aba8b0
Stephen Gutekanst <stephen@hexops.com>
2021-04-21 01:58:22 -0700

    ResultStream: detect and handle cyclic grammars which parse the empty language
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/combinator/gll_result_stream.zig
+++ src/combinator/gll_result_stream.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
 const testing = std.testing;
 const mem = std.mem;
+pub usingnamespace @import("gll_parser_path.zig");
 
 /// A ResultStream iterator.
 pub fn Iterator(comptime T: type) type {
     return struct {
         stream: *ResultStream(T),
         index: usize = 0,
+        subscriber_id: u64,
+        path: ParserPath,
 
         const Self = @This();
 
@@ -18,6 +21,19 @@ pub fn Iterator(comptime T: type) type {
             if (self.stream.past_values.items.len == 0 or self.index >= self.stream.past_values.items.len) {
                 if (self.stream.closed) {
                     return null; // no more results
+                }
+                if (self.path.contains(self.subscriber_id)) {
+                    // The parser waiting on these results (self.subscriber_id) is itself a part of
+                    // a larger path of parsers which depend on this result in order to produce a
+                    // result. This indicates a cyclic grammar which parses the empty language,
+                    // e.g. in the most simple form:
+                    //
+                    //  Expr = Expr;
+                    //  Grammar = Expr;
+                    //
+                    // In practice it may be a more complex form; but regardless this means that
+                    // the subscriber should recieve no results.
+                    return null;
                 }
                 // set ourselves up to be resumed later:
                 self.stream.listeners.append(@frame()) catch unreachable;
@@ -45,14 +61,16 @@ pub fn ResultStream(comptime T: type) type {
         past_values: std.ArrayList(T),
         listeners: std.ArrayList(anyframe),
         closed: bool,
+        source_id: u64,
 
         const Self = @This();
 
-        pub fn init(allocator: *mem.Allocator) !Self {
+        pub fn init(allocator: *mem.Allocator, source_id: u64) !Self {
             return Self{
                 .past_values = std.ArrayList(T).init(allocator),
                 .listeners = std.ArrayList(anyframe).init(allocator),
                 .closed = false,
+                .source_id = source_id,
             };
         }
 
@@ -90,20 +108,27 @@ pub fn ResultStream(comptime T: type) type {
         ///
         /// Uses of the returned iterator are valid for as long as the result stream is not
         /// deinitialized.
-        pub fn subscribe(self: *Self) Iterator(T) {
-            return Iterator(T){ .stream = self };
+        pub fn subscribe(self: *Self, subscriber_id: u64, path: ParserPath) Iterator(T) {
+            return Iterator(T){
+                .stream = self,
+                .subscriber_id = subscriber_id,
+                .path = path,
+            };
         }
     };
 }
 
 test "result_stream" {
     nosuspend {
-        var stream = try ResultStream(i32).init(testing.allocator);
+        const subscriber_id = 0;
+        const source_id = 0;
+        const path = ParserPath.init();
+        var stream = try ResultStream(i32).init(testing.allocator, source_id);
         defer stream.deinit();
 
         // Subscribe and begin to query a value (next() will suspend) before any values have been added
         // to the stream.
-        var sub1 = stream.subscribe();
+        var sub1 = stream.subscribe(subscriber_id, path);
         var sub1first = async sub1.next();
 
         // Add a value to the stream, our first subscription will get it.
@@ -122,7 +147,7 @@ test "result_stream" {
         testing.expectEqual(@as(?i32, null), sub1.next());
 
         // Now that the stream is closed, add a new subscription and confirm we get all prior values.
-        var sub2 = stream.subscribe();
+        var sub2 = stream.subscribe(subscriber_id, path);
         testing.expectEqual(@as(?i32, 1), sub2.next());
         testing.expectEqual(@as(?i32, 2), sub2.next());
         testing.expectEqual(@as(?i32, null), sub2.next());
