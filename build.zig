3ccc4f627900ae16e02c0518f91d91f7062177e6
Stephen Gutekanst <stephen@hexops.com>
2021-05-04 20:50:52 -0700

    rename main.zig -> zorex.zig
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -2,11 +2,11 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("template", "src/main.zig");
+    const lib = b.addStaticLibrary("template", "src/zorex.zig");
     lib.setBuildMode(mode);
     lib.install();
 
-    var main_tests = b.addTest("src/main.zig");
+    var main_tests = b.addTest("src/zorex.zig");
     main_tests.test_evented_io = true;
     main_tests.setBuildMode(mode);
 
