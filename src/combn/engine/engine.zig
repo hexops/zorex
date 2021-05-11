56b51aefac7b2d5893b21d79077014edc0fd3553
Stephen Gutekanst <stephen@hexops.com>
2021-05-11 05:03:05 -0700

    combn: add HeapParser for heap-allocated parser storage
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/combn/engine/engine.zig
+++ src/combn/engine/engine.zig
@@ -1,3 +1,4 @@
 pub usingnamespace @import("parser_path.zig");
 pub usingnamespace @import("parser.zig");
 pub usingnamespace @import("result_stream.zig");
+pub usingnamespace @import("heap_parser.zig");
