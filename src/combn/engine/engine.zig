ab305a10660d9d6534393271f9bccd7b3081f999
Stephen Gutekanst <stephen@hexops.com>
2021-05-11 05:59:13 -0700

    combn: remove HeapParser (prepare to make Parser itself heap-ready)
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/combn/engine/engine.zig
+++ src/combn/engine/engine.zig
@@ -1,4 +1,3 @@
 pub usingnamespace @import("parser_path.zig");
 pub usingnamespace @import("parser.zig");
 pub usingnamespace @import("result_stream.zig");
-pub usingnamespace @import("heap_parser.zig");
