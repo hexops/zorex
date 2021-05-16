e3ddaf5a5d80428c376fc2c28ef51f19f0aaf197
Stephen Gutekanst <stephen@hexops.com>
2021-05-16 00:38:46 -0700

    dsl: add Identifier parser
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ src/dsl/identifier.zig
@@ -0,0 +1,79 @@
+usingnamespace @import("../combn/combn.zig");
+
+const String = @import("String.zig");
+const Program = @import("Program.zig");
+
+const std = @import("std");
+const testing = std.testing;
+const mem = std.mem;
+
+/// Matches the identifier `input` string.
+///
+/// The `input` string must remain alive for as long as the `Identifier` parser will be used.
+pub const Identifier = struct {
+    parser: Parser(Program) = Parser(Program).init(parse, nodeName, null),
+
+    const Self = @This();
+
+    pub fn init() Self {
+        return Self{};
+    }
+
+    pub fn nodeName(parser: *const Parser(Program), node_name_cache: *std.AutoHashMap(usize, ParserNodeName)) Error!u64 {
+        const self = @fieldParentPtr(Self, "parser", parser);
+
+        var v = std.hash_map.hashString("Identifier");
+        return v;
+    }
+
+    pub fn parse(parser: *const Parser(Program), in_ctx: *const Context(void, Program)) callconv(.Async) !void {
+        const self = @fieldParentPtr(Self, "parser", parser);
+        var ctx = in_ctx.with({});
+        defer ctx.results.close();
+
+        const src = ctx.src[ctx.offset..];
+
+        var offset: usize = 0;
+        if (src.len == 0) {
+            try ctx.results.add(Result(Program).initError(ctx.offset, "expected Identifier"));
+            return;
+        }
+        {
+            var isUpper = src[offset] >= 'A' and src[offset] <= 'Z';
+            var isLower = src[offset] >= 'a' and src[offset] <= 'z';
+            if (!isUpper and !isLower) {
+                try ctx.results.add(Result(Program).initError(ctx.offset + 1, "Identifier must start with a-zA-Z"));
+                return;
+            }
+        }
+        while (offset < src.len) {
+            var isDigit = src[offset] >= '0' and src[offset] <= '9';
+            var isUpper = src[offset] >= 'A' and src[offset] <= 'Z';
+            var isLower = src[offset] >= 'a' and src[offset] <= 'z';
+            if (!isDigit and !isUpper and !isLower and src[offset] != '_') {
+                break;
+            }
+            offset += 1;
+        }
+        try ctx.results.add(Result(Program).init(ctx.offset + offset, Program{ .value = .{ .identifier = try String.init(ctx.allocator, src[0..offset]) } }));
+    }
+};
+
+test "identifier" {
+    nosuspend {
+        const allocator = testing.allocator;
+
+        var ctx = try Context(void, Program).init(allocator, "Grammar2", {});
+        defer ctx.deinit();
+
+        var l = Identifier.init();
+        try l.parser.parse(&ctx);
+        defer ctx.results.deinitAll(ctx.allocator);
+
+        var sub = ctx.results.subscribe(ctx.key, ctx.path, Result(Program).initError(ctx.offset, "matches only the empty language"));
+        var r1 = sub.next().?;
+        testing.expectEqual(@as(usize, 8), r1.offset);
+        testing.expectEqualStrings("Grammar2", r1.result.value.value.identifier.value.items);
+        testing.expect(sub.next() == null);
+    }
+}
