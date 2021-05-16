6b889e04fbaa549bad9aa8b5f0812beaefd329b5
Stephen Gutekanst <stephen@hexops.com>
2021-05-16 00:38:34 -0700

    dsl: add Node type
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ src/dsl/Node.zig
@@ -0,0 +1,23 @@
+//! The result of executing a Program is a tree of nodes describing the parsed language.
+
+const String = @import("String.zig");
+
+const std = @import("std");
+const mem = std.mem;
+
+name: String,
+value: ?String,
+children: ?std.ArrayList(@This()),
+
+pub fn deinit(self: *const @This(), allocator: *mem.Allocator) void {
+    self.name.deinit(allocator);
+    if (self.value) |v| {
+        v.deinit(allocator);
+    }
+    if (self.children) |children| {
+        for (children.items) |child| {
+            child.deinit(allocator);
+        }
+        children.deinit();
+    }
+}
