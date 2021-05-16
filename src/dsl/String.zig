e34767b354a5acec07a8a194707c2e729b35ee7c
Stephen Gutekanst <stephen@hexops.com>
2021-05-16 00:38:04 -0700

    dsl: add a String type
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ src/dsl/String.zig
@@ -0,0 +1,18 @@
+//! A heap-allocated string that is growable with dynamic capacity.
+
+const std = @import("std");
+const mem = std.mem;
+
+value: std.ArrayList(u8),
+
+pub fn init(allocator: *mem.Allocator, value: []const u8) !@This() {
+    var self = @This(){
+        .value = try std.ArrayList(u8).initCapacity(allocator, value.len),
+    };
+    try self.value.appendSlice(value);
+    return self;
+}
+
+pub fn deinit(self: *const @This(), allocator: *mem.Allocator) void {
+    self.value.deinit();
+}
