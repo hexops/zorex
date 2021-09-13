16107672f8cdacb13580d285a7db24bce1d42447
Stephen Gutekanst <stephen@hexops.com>
2021-09-12 23:25:05 -0700

    combn: eliminate usingnamespace from engine module
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/combn/engine/engine.zig
+++ src/combn/engine/engine.zig
@@ -1,3 +1,13 @@
 pub const ParserPath = @import("ParserPath.zig");
-pub usingnamespace @import("parser.zig");
-pub usingnamespace @import("result_stream.zig");
+
+pub const parser = @import("parser.zig");
+pub const Error = parser.Error;
+pub const ResultTag = parser.ResultTag;
+pub const Result = parser.Result;
+pub const ParserPosKey = parser.ParserPosKey;
+pub const ParserNodeName = parser.ParserNodeName;
+pub const Context = parser.Context;
+pub const Parser = parser.Parser;
+
+pub const result_stream = @import("result_stream.zig");
+pub const ResultStream = result_stream.ResultStream;
