b873fa5fc3930da887120c334b7a2d3c7180bbf0
Stephen Gutekanst <stephen@hexops.com>
2021-09-12 22:56:52 -0700

    combn: eliminate usingnamespace usage in parser module
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/combn/parser/parser.zig
+++ src/combn/parser/parser.zig
@@ -1,3 +1,14 @@
-pub usingnamespace @import("byte_range.zig");
-pub usingnamespace @import("end.zig");
-pub usingnamespace @import("literal.zig");
+pub const byte_range = @import("byte_range.zig");
+pub const ByteRange = byte_range.ByteRange;
+
+pub const end = @import("end.zig");
+pub const End = end.End;
+
+pub const literal = @import("literal.zig");
+pub const Literal = literal.Literal;
+
+test "include" {
+    _ = ByteRange;
+    _ = End;
+    _ = Literal;
+}
